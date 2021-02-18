6e24f3f5dd4908cfa3e8192563c4e1ff8fc448fd
Karpovich_alex_mpc <karpovehalex@gmail.com>
2021-02-18 19:28:08 +0300

    Table Changing Title saving with delay

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,19 +1,27 @@
 import {ExcelComponent} from '@core/ExcelComponent';
 import {$} from '@core/dom';
 import * as actions from '@/redux/actions'
+import {debounce} from '@core/utils';
 
 export class Header extends ExcelComponent {
-    static className = 'excel__header'
-    constructor($root, options) {
-      super($root, {
-        name: 'Header',
-        listeners: ['input', 'keydown'],
-        ...options
-      });
-    }
-    toHTML() {
-      const tableName=this.store.getState().tableName
-      return `
+  static className = 'excel__header'
+
+  constructor($root, options) {
+    super($root, {
+      name: 'Header',
+      listeners: ['input', 'keydown'],
+      ...options
+    });
+  }
+
+  prepare() {
+    super.prepare()
+    this.onInput = debounce(this.onInput, 300)
+  }
+
+  toHTML() {
+    const tableName = this.store.getState().tableName
+    return `
         <input type="text" class="input" value="${tableName}" />
 
       <div>
@@ -28,20 +36,20 @@ export class Header extends ExcelComponent {
 
       </div>
         `
-    }
+  }
 
-    onKeydown(event) {
-      const keys = ['Enter', 'Tab']
-      const {key} = event
-      if (keys.includes(key)) {
-        event.preventDefault()
-        this.$emit('formula:done')
-      }
+  onKeydown(event) {
+    const keys = ['Enter', 'Tab']
+    const {key} = event
+    if (keys.includes(key)) {
+      event.preventDefault()
+      this.$emit('formula:done')
     }
+  }
 
-    onInput(event) {
-      const text = $(event.target).text()
-      console.log(text)
-      this.$dispatch(actions.changeTableName(text))
-    }
+  onInput(event) {
+    const text = $(event.target).text()
+    console.log(text)
+    this.$dispatch(actions.changeTableName(text))
+  }
 }
